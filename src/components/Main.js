be449f4cf2c844742ecdf198f5312d7c7288c52a
Brant <boshartb@gmail.com>
2019-01-27 13:52:32 -0500

    Added beer counter with state

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 
-import Header from './Header';
-import Beer from './Beer';
+import Results from "./Results";
+import Search from "./Search";
+import Header from "./Header";
 
 
 class Main extends Component {
+    constructor() {
+        super();
+        this.state = {
+            numBeers: 10,
+            beers: []
+        }
+    }
+
+    incrementBeers = () => {
+        // create a new upated state variable
+        const beerAmount = this.state.numBeers + 1;
+        // set state to that amount
+        this.setState({ numBeers: beerAmount });
+    };
+
     render() {
         return (
             <div className="wrapper">
                 <Header siteName="Beer Me! 🍻" />
-                <Beer />
+                <button onClick={this.incrementBeers}>{this.state.numBeers} 🍺</button>
+
+                <Search />
+                <Results />
             </div>
         )
     }
