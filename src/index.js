9453a8a462362a85a22404ecd3dbab36da695117
Brant <boshartb@gmail.com>
2019-01-27 12:05:22 -0500

    inital set up and component

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+/* Import CSS */
+import css from './style.css';
+
+import Header from './components/Header';
+
+
+render(<Header />, document.querySelector('#root'));
